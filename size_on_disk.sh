13ad4fb4764fac0a09c7ca485c8623d12860f7e1
Ryan H <git@dredx.com>
2025-05-19 20:24:23 -0700

    added size totals for Manuals and Videos

--- size_on_disk.sh
+++ size_on_disk.sh
@@ -6,32 +6,33 @@ MEDIA_DIR="/mnt/user/Stuff/ES-DE/ES-DE/downloaded_media"
 
 format_bytes_align() {
   local bytes=$1
-  if (( bytes >= 1073741824  )); then
-    awk -v b="$bytes" 'BEGIN { printf "%7.2f G", b / (1024*1024*1024) }'
+  if (( bytes >= 1073741824 )); then
+    awk -v b="$bytes" 'BEGIN { printf "%.2f G", b / (1024*1024*1024) }'
   elif (( bytes >= 1048576 )); then
-    awk -v b="$bytes" 'BEGIN { printf "%7.0f M", b / (1024*1024) }'
+    awk -v b="$bytes" 'BEGIN { printf "%.0f M", b / (1024*1024) }'
   else
-    awk -v b="$bytes" 'BEGIN { printf "%7.0f K", b / 1024 }'
+    awk -v b="$bytes" 'BEGIN { printf "%.0f K", b / 1024 }'
   fi
 }
 
 # Set up total counters (in bytes)
 rom_total=0
 media_total=0
+image_total=0
+video_total=0
+manual_total=0
 sort_field="none"
 basic_output=0
 declare -a system_filters=()
 
 # Argument parsing
-# Support short sort aliases: --sort-r, --sort-m, --sort-t
-# Also allow overriding ROM and media root paths
 while [[ $# -gt 0 ]]; do
   case "$1" in
     --sort=rom|--sort=r)   sort_field="rom" ;;
     --sort=media|--sort=m) sort_field="media" ;;
     --sort=total|--sort=t) sort_field="total" ;;
-    --sort)       sort_field="total" ;;
-    --basic)      basic_output=1 ;;
+    --sort)                sort_field="total" ;;
+    --basic)               basic_output=1 ;;
     --system)
       shift
       IFS=',' read -ra system_filters <<< "$1"
@@ -53,18 +54,14 @@ while [[ $# -gt 0 ]]; do
     --media-folder=*)
       MEDIA_DIR="${1#*=}"
       ;;
-
     --help|-h)
-      echo "Usage: $0 [--sort[=rom|media|total]] [--basic] [-s <system1,system2>] [--system=nes,snes] [--rom-folder path] [--media-folder path]"
-      echo "  --rom-folder     Path to ROM root directory"
-      echo "  --media-folder   Path to media root directory"
-      echo "  --sort           Default to sorting by total size"
-      echo "  --sort=rom       Sort by ROM size (also --sort=r)"
-      echo "  --sort=media     Sort by media size (also --sort=m)"
-      echo "  --sort=total     Sort by total size (also --sort=t)"
-      echo "  --basic          Output basic colon-separated data (suitable for piping to a file)"
-      echo "  --system         Comma-separated list of systems to process"
-      echo "  --help           Show this message"
+      echo "Usage: $0 [options]"
+      echo "  --rom-folder              Path to ROM root directory"
+      echo "  --media-folder            Path to media root directory"
+      echo "  --sort[=rom|media|total]  Sort output by size field"
+      echo "  --basic                   Output colon-separated values for scripting"
+      echo "  --system=nes,snes         Comma-separated list of systems"
+      echo "  --help                    Show this help message"
       exit 0
       ;;
     *)
@@ -79,7 +76,6 @@ done
 # Create a temporary file to collect sortable data
 tempfile=$(mktemp)
 
-# Initial status message
 [[ $basic_output -eq 0 ]] && echo -ne "Processing...\r"
 
 # Build directory list
@@ -105,10 +101,17 @@ for d in "${rom_dirs[@]}"; do
   [[ $basic_output -eq 0 ]] && echo -ne "Processing: $dirname         \r"
 
   rom_kb=$(du -sL "$d" 2>/dev/null | awk '{print $1}')
-  media_kb=$(du -sL "$media_dir" 2>/dev/null | awk '{print $1}')
-  media_kb=${media_kb:-0}
+  media_kb_full=$(du -sL "$media_dir" 2>/dev/null | awk '{print $1}')
+  manuals_kb=$(du -sL "$media_dir/manuals" 2>/dev/null | awk '{print $1}')
+  videos_kb=$(du -sL "$media_dir/videos" 2>/dev/null | awk '{print $1}')
+  media_kb_images=$((media_kb_full - manuals_kb - videos_kb))
 
-  # Only skip small folders (≤ 4KB) if not manually specified
+  rom_kb=${rom_kb:-0}
+  media_kb_full=${media_kb_full:-0}
+  manuals_kb=${manuals_kb:-0}
+  videos_kb=${videos_kb:-0}
+  media_kb_images=$(( media_kb_images >= 0 ? media_kb_images : 0 ))
+
   if [[ $rom_kb -le 4 && ${#system_filters[@]} -eq 0 ]]; then
     continue
   fi
@@ -119,57 +122,51 @@ for d in "${rom_dirs[@]}"; do
     rom_bytes=$((rom_kb * 1024))
   fi
 
-  media_bytes=$((media_kb * 1024))
+  media_bytes=$((media_kb_full * 1024))
+  image_bytes=$((media_kb_images * 1024))
+  video_bytes=$((videos_kb * 1024))
+  manual_bytes=$((manuals_kb * 1024))
   total_bytes=$((rom_bytes + media_bytes))
 
   rom_total=$((rom_total + rom_bytes))
   media_total=$((media_total + media_bytes))
+  image_total=$((image_total + image_bytes))
+  video_total=$((video_total + video_bytes))
+  manual_total=$((manual_total + manual_bytes))
 
   rom_human=$(format_bytes_align "$rom_bytes")
-  media_human=$(format_bytes_align "$media_bytes")
+  image_human=$(format_bytes_align "$image_bytes")
+  video_human=$(format_bytes_align "$video_bytes")
+  manual_human=$(format_bytes_align "$manual_bytes")
   total_human=$(format_bytes_align "$total_bytes")
 
-  printf "%d\t%d\t%d\t%s\t%s\t%s\t%s\n" "$rom_bytes" "$media_bytes" "$total_bytes" "$dirname" "$rom_human" "$media_human" "$total_human" >> "$tempfile"
+  printf "%s\t%s\t%s\t%s\t%s\t%s\n" "$dirname" "$rom_human" "$image_human" "$video_human" "$manual_human" "$total_human" >> "$tempfile"
 done
 
 [[ $basic_output -eq 0 ]] && echo -ne "                                         \r"
 
 # Output
 if [[ $basic_output -eq 1 ]]; then
-  awk -F'\t' '{ printf "%s:%d:%d:%d\n", $4, $1 / 1024, $2 / 1024, $3 / 1024 }' "$tempfile"
-  if [[ ${#system_filters[@]} -ne 1 ]]; then
-    rom_kb_total=$((rom_total / 1024))
-    media_kb_total=$((media_total / 1024))
-    total_kb_combined=$(((rom_total + media_total) / 1024))
-    echo "TOTAL:$rom_kb_total:$media_kb_total:$total_kb_combined"
-  fi
+  awk -F'\t' '{ printf "%s:%s:%s:%s:%s:%s\n", $1, $2, $3, $4, $5, $6 }' "$tempfile"
 else
   echo
-  printf "%-15s %-12s %-12s %-12s\n" "System" "Rom Size" "Media Size" "Total Size"
-  printf "%-15s %-12s %-12s %-12s\n" "--------------" "----------" "----------" "----------"
+  printf "%-15s %-10s %-10s %-10s %-10s %-12s\n" "System" "Roms" "Images" "Videos" "Manuals" "Total Size"
+  printf "%-15s %-10s %-10s %-10s %-10s %-12s\n" "--------------" "--------" "--------" "--------" "--------" "------------"
 
-  case "$sort_field" in
-    rom|r)
-      sort -nr -k1 "$tempfile"
-      ;;
-    media|m)
-      sort -nr -k2 "$tempfile"
-      ;;
-    total|t)
-      sort -nr -k3 "$tempfile"
-      ;;
-    *)
-      cat "$tempfile"
-      ;;
-  esac | awk -F'\t' '{ printf "%-15s %-12s %-12s %-12s\n", $4, $5, $6, $7 }'
+  cat "$tempfile" | awk -F'\t' '{ printf "%-15s %8s   %8s   %8s   %8s   %12s\n", $1, $2, $3, $4, $5, $6 }'
 
   if [[ ${#system_filters[@]} -ne 1 ]]; then
     echo
-    printf "%-15s %s\n" "Total ROMs:"     "$(format_bytes_align "$rom_total")"
-    printf "%-15s %s\n" "Total Media:"    "$(format_bytes_align "$media_total")"
-    printf "%-15s %s\n" "Combined Total:" "$(format_bytes_align "$((rom_total + media_total))")"
+    printf "%-20s %s\n" "Roms:"              "$(format_bytes_align "$rom_total")"
+    printf "%-20s %s\n" "Images:"            "$(format_bytes_align "$image_total")"
+    printf "%-20s %s\n" "Videos:"            "$(format_bytes_align "$video_total")"
+    printf "%-20s %s\n" "Manuals:"           "$(format_bytes_align "$manual_total")"
+    printf "%-20s %s\n" "All Media:"         "$(format_bytes_align "$media_total")"
+    printf "%-20s %s\n" "Roms + Images:"     "$(format_bytes_align "$((rom_total + image_total))")"
+    printf "%-20s %s\n" "Roms + All Media:"  "$(format_bytes_align "$((rom_total + media_total))")"
+    echo
   fi
-  echo
+
 fi
 
 # Clean up
