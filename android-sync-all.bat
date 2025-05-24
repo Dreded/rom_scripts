23a3d1972ac3114a303a27b0cb96c09de36507e9
Ryan H <git@dredx.com>
2025-05-23 17:00:30 -0700

    added some android syncing through adb-sync

---
+++ android-sync-all.bat
@@ -0,0 +1,3 @@
+@echo off
+python adb-sync.py --delete ../ES-DE/ /sdcard/ES-DE
+python adb-sync.py --delete ../ROMs/ /sdcard/ROMs
--- android-sync.bat
+++ android-sync.bat
@@ -1,3 +1,32 @@
 @echo off
-python adb-sync.py ./ROMs/ /sdcard/ROMs/
-python adb-sync.py ./ES-DE/ /sdcard/ES-DE/
+setlocal enabledelayedexpansion
+
+:: Define source and target
+set "ESDE_SRC=..\ES-DE"
+set "ESDE_DST=/sdcard/ES-DE"
+
+:: Sync ROMs
+rem python adb-sync.py --delete ../ROMs/ /sdcard/ROMs/
+
+:: Sync everything in ES-DE except downloaded_media
+for /d %%D in ("%ESDE_SRC%\*") do (
+    if /I not "%%~nxD"=="downloaded_media" (
+        set "SRC=%%~fD"
+        set "SRC=!SRC:\=/!"
+        python adb-sync.py --delete "!SRC!" "%ESDE_DST%/%%~nxD"
+    )
+)
+
+:: Handle downloaded_media separately
+if exist "%ESDE_SRC%\downloaded_media" (
+    for /d %%D in ("%ESDE_SRC%\downloaded_media\*") do (
+        set "FOLDER=%%~nxD"
+        if /I not "!FOLDER!"=="videos" if /I not "!FOLDER!"=="manuals" (
+            set "SRC=%%~fD"
+            set "SRC=!SRC:\=/!"
+            python adb-sync.py --delete "!SRC!" "%ESDE_DST%/downloaded_media/!FOLDER!"
+        )
+    )
+)
+
+endlocal
