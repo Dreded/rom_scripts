b82900559e314c351b01bdaefe0a570c242be93a
Ryan H <git@dredx.com>
2025-05-23 17:01:13 -0700

    mapped EmulationStation to onionos rom folder names to make copying to miyoomini easier

---
+++ miyoo_copy.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+SRC="/mnt/Stuff/ES-DE/ROMs"
+DST="/mnt/g/roms"
+
+# Source-to-destination system map
+declare -A map=(
+  [atari2600]=ATARI
+  [atari5200]=FIFTYTWOHUNDRED
+  [atari7800]=SEVENTYEIGHTHUNDRED
+  [cps1]=CPS1
+  [cps2]=CPS2
+  [cps3]=CPS3
+  [fbneo]=ARCADE
+  [gamegear]=GG
+  [gb]=GB
+  [gba]=GBA
+  [gbc]=GBC
+  [genesis]=MD
+  [gw]=GW
+  [lynx]=LYNX
+  [mastersystem]=MS
+  [nds]=NDS
+  [neocd]=NEOCD
+  [neogeo]=NEOGEO
+  [nes]=FC
+  [ngp]=NGP
+  [pcecd]=PCECD
+  [pico]=PICO
+  [ports]=PORTS
+  [psx]=PS
+  [scummvm]=SCUMMVM
+  [sega32x]=THIRTYTWOX
+  [segacd]=SEGACD
+  [snes]=SFC
+  [supergrafx]=SGFX
+  [tg16]=PCE
+  [ws]=WS
+)
+
+# Files to exclude from deletion
+excludes=(
+  "miyoogamelist.xml.bak"
+  "~Filter.miyoocmd"
+  "~Refresh roms.miyoocmd"
+)
+
+# Build rsync exclude options
+exclude_args=()
+for pattern in "${excludes[@]}"; do
+  exclude_args+=(--exclude="$pattern")
+done
+
+# Sync logic
+for sys in "${!map[@]}"; do
+  src_path="$SRC/$sys"
+  dst_path="$DST/${map[$sys]}"
+
+  if [[ ! -d "$src_path" ]] || [[ -z "$(find "$src_path" -type f -not -name '.*' -print -quit)" ]]; then
+    echo "Skipping $sys (empty or missing)"
+    continue
+  fi
+
+  echo "Syncing $src_path -> $dst_path"
+  rsync -av --size-only "${exclude_args[@]}" --delete "$src_path/" "$dst_path/"
+done
+
+# Clean up miyoogamelist.xml files
+echo "Cleaning XML files in destination..."
+for file in "$DST"/*/miyoogamelist.xml; do
+  if [[ -f "$file" ]]; then
+    echo "Cleaning $file"
+    xmlstarlet ed -L -d '//game/*[not(self::path or self::name or self::image)]' "$file"
+  fi
+done
